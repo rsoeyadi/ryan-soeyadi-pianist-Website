6ce04ee412531aed2009312451ef8257b81b7d2d
Ryan Soeyadi <rsoeyadi@me.com>
2021-03-03 22:29:00 -0500

    Toggle display-none class

--- burger.js
+++ burger.js
@@ -1,5 +1,8 @@
 var hamburger = document.querySelector('.hamburger');
+var nav_bar = document.querySelector('.nav-bar')
 
 hamburger.onclick = function() {
     hamburger.classList.toggle("is-active")
+    nav_bar.classList.toggle("display-none")
+    
   }
