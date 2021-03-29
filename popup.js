7dd4f66fc831e6b694b5c247e060f4abd751a8e6
Ryan Soeyadi <rsoeyadi@me.com>
2021-03-29 12:13:15 -0400

    Styling

--- popup.js
+++ popup.js
@@ -2,7 +2,15 @@
 //and the script is ready to be executed.
 $(document).ready(function ()  
 {
-//select the close by class and apply a click even listener
+
+    if(sessionStorage.getItem('#modal') !== 'true'){
+        $('#modal').css('display','block');
+    //then the modal will be set true in the current session due to which the modal won't 
+    //reappear on the refresh, we need to reload the page in a new tab to make the modal 
+    //reappear.
+    sessionStorage.setItem('#ad_modal','true');
+        }
+    //select the close by class and apply a click even listener
   $(".close").on("click", function () 
 	{
 		//select the modal element by id , and apply display none when close is clicked
